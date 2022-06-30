e892288cab6e13829c7127357c0938a9407c23f2
diwakar <50358405+diwakar2000@users.noreply.github.com>
2022-06-30 15:39:58 +0545

    Initial Commit
    Initial Commit

---
+++ addonify-floating-cart.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://addonify.com/
+ * @since             1.0.0
+ * @package           Addonify_Floating_Cart
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Addonify Floating Cart
+ * Plugin URI:        https://addonify.com/addonify-floating-cart
+ * Description:       
+ * Version:           1.0.0
+ * Author:            Addonify
+ * Author URI:        https://addonify.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       addonify-floating-cart
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'ADDONIFY_FLOATING_CART_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-addonify-floating-cart-activator.php
+ */
+function activate_addonify_floating_cart() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-addonify-floating-cart-activator.php';
+	Addonify_Floating_Cart_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-addonify-floating-cart-deactivator.php
+ */
+function deactivate_addonify_floating_cart() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-addonify-floating-cart-deactivator.php';
+	Addonify_Floating_Cart_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_addonify_floating_cart' );
+register_deactivation_hook( __FILE__, 'deactivate_addonify_floating_cart' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-addonify-floating-cart.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_addonify_floating_cart() {
+
+	$plugin = new Addonify_Floating_Cart();
+	$plugin->run();
+
+}
+run_addonify_floating_cart();

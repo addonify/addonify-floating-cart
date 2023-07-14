e9dcfa5bfc75cf337ef7a5fb78c38b07d5aabe64
Anuj Subedi <struggleage@gmail.com>
2023-07-14 12:30:29 +0545

    🎮 NEW: Option to hide toggle button.
    - Added: Option, Hide trigger button if the cart is empty. #221

--- public/partials/floating-button.php
+++ public/partials/floating-button.php
@@ -9,14 +9,36 @@
  */
 
 defined( 'ABSPATH' ) || exit;
+
+/**
+* Function that returns button [data_display] attribute value.
+*
+* @since 1.1.9
+*/
+function display_button() {
+
+	$display_val = 'visible';
+
+	if ( addonify_floating_cart_get_option( 'hide_modal_toggle_button_on_empty_cart' ) ) {
+
+		if ( WC()->cart->get_cart_contents_count() === 0 ) {
+
+			$display_val = 'hidden';
+		}
+	}
+
+	return $display_val;
+}
 ?>
-<button id="adfy__woofc-trigger" class="adfy__show-woofc <?php echo esc_attr( $position ); ?>" data_display="visible">
+<button 
+	id="adfy__woofc-trigger" 
+	class="adfy__show-woofc <?php echo esc_attr( $position ); ?>" 
+	data_display="<?php echo esc_attr( display_button() ); ?>">
 	<?php
 	if ( $button_icon ) {
 		echo '<span class="icon">' . $button_icon . '</span>'; // phpcs:ignore
 	}
 	?>
-	</span>
 	<?php if ( 1 === $display_badge ) { ?>
 		<?php
 		if ( addonify_floating_cart_get_option( 'cart_badge_items_total_count' ) === 'total_products' ) {
@@ -25,7 +47,9 @@ defined( 'ABSPATH' ) || exit;
 			$cart_items_count = WC()->cart->get_cart_contents_count();
 		}
 		?>
-		<span class="badge <?php echo esc_attr( $badge_position ); ?>"><span class="adfy_woofc-badge-count"><?php echo esc_html( $cart_items_count ); ?></span></span>
+		<span class="badge <?php echo esc_attr( $badge_position ); ?>">
+			<span class="adfy_woofc-badge-count"><?php echo esc_html( $cart_items_count ); ?></span>
+		</span>
 	<?php } ?>
 </button>
 <?php
